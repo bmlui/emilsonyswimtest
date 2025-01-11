d087619d5ad8e62dc62e7cd3a6c08f5c45b3ba2b
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2025-01-10 22:46:01 -0500

    move footer to own component, and adjust height

--- swimtestapp/app/layout.tsx
+++ swimtestapp/app/layout.tsx
@@ -14,7 +14,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Swim Test — Emilson Y",
-  description: "Swim test logger app for South Shore YMCA Emilson Branch in Hanover, MA",
+  description:
+    "Swim test logger app for South Shore YMCA Emilson Branch in Hanover, MA",
 };
 
 export default function RootLayout({
@@ -22,23 +23,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
-
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="relative">
-        
-          {children}
-        </div>
-        <footer className="text-center text-gray-500 mb-5 ml-5 mr-5">
-        A Culture of Safety. A Commitment to Care. A Standard of Excellence.<br/>
-          Data is synced with the Google Sheet on the supervisor account.<br/>
-          For support, please email <a href="mailto:contact@brandonlui.com" className="text-blue-500 underline">contact@brandonlui.com</a> or call <a href="tel:+18024172584" className="text-blue-500 underline ">+1 (802) 417-BLUI</a> (1-802-417-2584).
-        </footer>
-        <div className="min-h-screen"></div>
+        <div className="relative">{children}</div>
       </body>
     </html>
   );
