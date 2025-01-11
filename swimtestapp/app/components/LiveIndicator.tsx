86782fed28c509739a88b53522672010b82c3d49
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2025-01-10 20:06:51 -0500

    add tester name validation, and adjust websocket

--- swimtestapp/app/components/LiveIndicator.tsx
+++ swimtestapp/app/components/LiveIndicator.tsx
@@ -1,39 +1,59 @@
-import React from 'react';
-
+import React from "react";
+import { useEffect, useState } from "react";
 interface LiveIndicatorProps {
-    isConnected: boolean;
+  isConnected: boolean;
 }
 
 const LiveIndicator: React.FC<LiveIndicatorProps> = ({ isConnected }) => {
+  const [showLiveIndicator, setShowLiveIndicator] = useState<boolean>(false);
 
-    const onReconnect = () => {
-        window.location.reload();
-    };
-    return (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div
-                className={`w-2.5 h-2.5 rounded-full mr-2 ${isConnected ? 'bg-green-500' : 'bg-red-500'}`}
-            ></div>
-            <span className="mr-2">{isConnected ? 'Live connection' : 'Offline mode'}</span>
-            {!isConnected && (
-                <button onClick={onReconnect} className="text-blue-500 text-sm flex items-center hover:underline">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-4 w-4 mr-1"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M4 4v6h6M20 20v-6h-6M4 20l16-16"/>
-                    </svg>
-                  Reconnect
-                </button>
-            )}
+  useEffect(() => {
+    if (isConnected) {
+      setShowLiveIndicator(true);
+    }
+  }, [isConnected]);
+
+  const onReconnect = () => {
+    window.location.reload();
+  };
+  return (
+    <>
+      {showLiveIndicator && (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div
+            className={`w-2.5 h-2.5 rounded-full mr-2 ${
+              isConnected ? "bg-green-500" : "bg-red-500"
+            }`}
+          ></div>
+          <span className="mr-2">
+            {isConnected ? "Online" : "Offline mode"}
+          </span>
+          {!isConnected && (
+            <button
+              onClick={onReconnect}
+              className="text-blue-500 text-sm flex items-center hover:underline"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4 mr-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 4v6h6M20 20v-6h-6M4 20l16-16"
+                />
+              </svg>
+              Reconnect
+            </button>
+          )}
         </div>
-    );
+      )}
+    </>
+  );
 };
 
 export default LiveIndicator;
--- swimtestapp/app/page.tsx
+++ swimtestapp/app/page.tsx
@@ -1,11 +1,11 @@
-'use client';
-import { useCallback, useEffect, useState } from 'react';
-import AddDataForm from './components/AddDataForm';
-import SearchBar from './components/SearchBar';
-import SwimTestList from './components/SwimTestList';
-import FetchSwimTestData from './components/FetchSwimTestData';
-import Header from './components/Header';
-import LiveIndicator from './components/LiveIndicator';
+"use client";
+import { useCallback, useState } from "react";
+import AddDataForm from "./components/AddDataForm";
+import SearchBar from "./components/SearchBar";
+import SwimTestList from "./components/SwimTestList";
+import FetchSwimTestData from "./components/FetchSwimTestData";
+import Header from "./components/Header";
+import LiveIndicator from "./components/LiveIndicator";
 
 export interface SwimTestData {
   firstName: string;
@@ -21,24 +21,6 @@ export default function Home() {
   const [filteredData, setFilteredData] = useState<SwimTestData[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isConnected, setIsConnected] = useState<boolean>(false);
-  const [showLiveIndicator, setShowLiveIndicator] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (isConnected) {
-      setShowLiveIndicator(true);
-    }
-  }, [isConnected]);
-
-  const handleSearch = (query: string) => {
-    const filtered = data.filter((item) =>
-      item.fullName.includes(query.replace(/[^a-zA-Z]/g, '').toUpperCase())
-    );
-    if (query === '') {
-      setFilteredData(data);
-      return;
-    }
-    setFilteredData(filtered);
-  };
 
   const addDataLocal = useCallback((newData: SwimTestData) => {
     setData((prevData) => {
@@ -59,25 +41,31 @@ export default function Home() {
 
   return (
     <div>
-      <FetchSwimTestData  setData={setData} setFilteredData={setFilteredData} setIsLoading={setIsLoading} onAdd={addDataLocal} setIsConnected={setIsConnected}/>
-    <div className="p-4 space-y-4">
-    <Header />
-      <div className=" max-w-xs mx-auto p-4 bg-gray-100 rounded-lg">
-        <h2 className="text-xl font-bold mb-3">Add Swim Test Data</h2>
-        <AddDataForm onAdd={addDataLocal} data={data}/>
+      <FetchSwimTestData
+        setData={setData}
+        setFilteredData={setFilteredData}
+        setIsLoading={setIsLoading}
+        onAdd={addDataLocal}
+        setIsConnected={setIsConnected}
+      />
+      <div className="p-4 space-y-4">
+        <Header />
+        <div className=" max-w-xs mx-auto p-4 bg-gray-100 rounded-lg">
+          <h2 className="text-xl font-bold mb-3">Add Swim Test Data</h2>
+          <AddDataForm onAdd={addDataLocal} data={data} />
+        </div>
+        <div className="p-4 space-y-4 rounded-lg bg-gray-100 max-w-4xl mx-auto">
+          <LiveIndicator isConnected={isConnected} />
+          <SearchBar data={data} setFilteredData={setFilteredData} />
+          {isLoading ? (
+            <div className="flex justify-center items-center">
+              <div className="animate-spin rounded-full h-8 w-8 border-4 border-gray-300 border-t-transparent"></div>
+            </div>
+          ) : (
+            <SwimTestList data={filteredData} />
+          )}
+        </div>
       </div>
-      <div className="p-4 space-y-4 rounded-lg bg-gray-100 max-w-4xl mx-auto">
-        { showLiveIndicator && <LiveIndicator isConnected={isConnected} />}
-        <SearchBar onSearch={handleSearch} />
-        {isLoading ? (
-          <div className="flex justify-center items-center">
-            <div className="animate-spin rounded-full h-8 w-8 border-4 border-gray-300 border-t-transparent"></div>
-          </div>
-        ) : (
-          <SwimTestList data={filteredData} />
-        )}
-      </div>
-    </div>
     </div>
   );
 }
