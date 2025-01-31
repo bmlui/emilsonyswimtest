ca4239cab14cbfa878b5ed9be2f355b28ae71f28
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2025-01-31 14:18:49 -0500

    add current search display

--- swimtestapp/app/components/SearchBar.tsx
+++ swimtestapp/app/components/SearchBar.tsx
@@ -36,23 +36,32 @@ export default function SearchBar({
   };
 
   return (
-    <div className="flex space-x-2">
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyDown={handleKeyPress}
-        placeholder="Search..."
-        className="flex-grow p-2 border border-gray-300 rounded"
-      />
-      <button
-        onClick={isSearched ? handleClear : handleAction}
-        className={`px-4 py-2 rounded hover:opacity-80 ${
-          isSearched ? "bg-gray-500" : "bg-blue-500"
-        } text-white`}
-      >
-        {isSearched ? "Clear" : "Search"}
-      </button>
+    <div>
+      <div className="flex space-x-2">
+        <input
+          type="search"
+          autoComplete="off"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={handleKeyPress}
+          placeholder="Search..."
+          className="flex-grow p-2 border border-gray-300 rounded"
+        />
+        <button
+          onClick={isSearched ? handleClear : handleAction}
+          className={`px-4 py-2 rounded hover:opacity-80 ${
+            isSearched ? "bg-gray-500" : "bg-blue-500"
+          } text-white`}
+        >
+          {isSearched ? "Clear" : "Search"}
+        </button>
+      </div>
+      {isSearched && (
+        <div className="mt-2 ">
+          Current Search:
+          <span className="font-bold"> {searchTerm}</span>
+        </div>
+      )}
     </div>
   );
 }
